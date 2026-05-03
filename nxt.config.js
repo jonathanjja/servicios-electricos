b848461fe0636614b9572f66111c7c4786da4229
jonathanjja <107899654+jonathanjja@users.noreply.github.com>
2026-05-03 13:56:51 -0600

    Create nxt.config.js

---
+++ nxt.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/servicios-electricos',
+  assetPrefix: '/servicios-electricos/',
+};
+
+module.exports = nextConfig;
